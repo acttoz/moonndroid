2a24df5a8cfae6753322a0a3d0cbf394fa35405e
acttoz <acttoz@12b37519-7715-0510-82f6-ba5b270cff09>
2015-05-21 13:29:31 +0000

    git-svn-id: https://dev.naver.com/svn/moonndroid@14 12b37519-7715-0510-82f6-ba5b270cff09

---
+++ AlgoriBoard/AlgoriBoard/Assets/00prefab/backkey.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backkey : MonoBehaviour
+{
+		public string LEVEL;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Input.GetKeyDown (KeyCode.Escape)) {
+
+						if (LEVEL.Equals ("EXIT")) {
+								Application.Quit ();
+						} else {
+								Application.LoadLevel (LEVEL);
+						}
+
+
+				}
+		}
+}
