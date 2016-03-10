90a0652819305a1dccbf5bb2aeb2408c9cd46da9
acttoz <acttoz@12b37519-7715-0510-82f6-ba5b270cff09>
2016-03-10 03:23:51 +0000

    git-svn-id: https://dev.naver.com/svn/moonndroid@21 12b37519-7715-0510-82f6-ba5b270cff09

--- Alim/_index.php
+++ Alim/_index.php
@@ -5,9 +5,6 @@ session_start();
 <!DOCTYPE html>
 <html lang="ko">
    <head>
-      <?
-	include_once ('./framework.php');
-         ?>
       <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
       <script src="//code.jquery.com/jquery-1.10.2.js"></script>
       <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
